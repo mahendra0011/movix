bf2bb2547c0225216e63b3377fec5b8f2a3455e1
Mahendra Prajapati <sourceforget32@gmail.com>
2026-05-30 18:05:54 +0530

    Add static fallbacks for detail routes

--- scripts/create-spa-route-fallbacks.mjs
+++ scripts/create-spa-route-fallbacks.mjs
@@ -1,6 +1,7 @@
 import { copyFile, mkdir } from "node:fs/promises";
 import { join } from "node:path";
 import { fileURLToPath, URL } from "node:url";
+import { comingSoonMovies, movies, theaters } from "../src/features/movies/data/movieCatalog.js";
 
 const distDir = new URL("../dist/", import.meta.url);
 const indexFile = new URL("index.html", distDir);
@@ -17,11 +18,19 @@ const routes = [
   "sports",
   "wishlist",
 ];
+const dynamicRoutes = [
+  ...movies.map((movie) => `movies/${movie.id}`),
+  ...comingSoonMovies.map((movie) => `movies/${movie.id}`),
+  ...theaters.map((theater) => `cinemas/${theater.id}`),
+];
+const fallbackRoutes = [...new Set([...routes, ...dynamicRoutes].filter(Boolean))];
 
 await Promise.all(
-  routes.map(async (route) => {
+  fallbackRoutes.map(async (route) => {
     const routeDir = new URL(`${route}/`, distDir);
     await mkdir(routeDir, { recursive: true });
     await copyFile(indexFile, join(fileURLToPath(routeDir), "index.html"));
   }),
 );
+
+await copyFile(indexFile, new URL("404.html", distDir));
