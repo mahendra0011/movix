54e66f4454fe99f2067a3db503428b15634106d0
mahendra <mahendrapra0077@gmail.com>
2026-07-05 21:15:22 +0530

    register city and UI slices in Redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "@/features/auth/authSlice";
+import { uiReducer } from "@/features/ui/uiSlice";
+import { cityReducer } from "@/features/city/citySlice";
+import { baseApi } from "@/features/api/baseApi";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    ui: uiReducer,
+    city: cityReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 export { store };
